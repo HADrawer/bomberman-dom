77a33f97d61147b88f8de8640dcf4d107dbdf774
falmarzo <faisalalmarzouqi25@gmail.com>
2025-09-02 02:37:54 +0300

    feat: added the intial grid and a couple of starting desgins

--- backend/main.go
+++ backend/main.go
@@ -7,22 +7,31 @@ import (
 )
 
 func main() {
-
 	mux := http.NewServeMux()
 
-	// Serve static files
+	// Serve static files (CSS, images, etc.)
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../Frontend/static"))))
 
-	// Serve index.html
+	// Serve game folder (grid.js)
+	mux.Handle("/game/", http.StripPrefix("/game/", http.FileServer(http.Dir("../Frontend/game"))))
+
+	// Serve Framework folder (app.js, v-node.js, etc.)
+	mux.Handle("/Framework/", http.StripPrefix("/Framework/", http.FileServer(http.Dir("../Frontend/Framework"))))
+
+	// Serve app.js
+	mux.Handle("/app.js", http.StripPrefix("/", http.FileServer(http.Dir("../Frontend"))))
+
+	// Serve index.html at root
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
 		http.ServeFile(w, r, "../Frontend/index.html")
 	})
 
-	// WebSocket route
-	// mux.HandleFunc("/ws", wsHandler)
-
 	port := ":8080"
-	fmt.Println(" Server is running on http://localhost" + port)
+	fmt.Println("Server running on http://localhost" + port)
 	if err := http.ListenAndServe(port, mux); err != nil {
 		log.Fatal("Failed to start server: ", err)
 	}
